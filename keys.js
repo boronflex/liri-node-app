e7278999d9b08f3634bfb23b3f87ad0f09b1b018
boronflex <kyle_palmer@icloud.com>
2017-11-30 09:50:46 -0600

    got to step 7

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '4bshJDMVsTUNxKVnPVyIJV5SR',
+  consumer_secret: 'gjeH0O3gbIK9HvQtL0eiYyYqjnc9cxOjlAPdptpIhPlCIf10mL',
+  access_token_key: '936257258698280960-BvWBtnB3ZYEY2CeSIs9RT3UpFcQeTwF',
+  access_token_secret: 'gDDPwIip52Dsff5NDu3iznsTotGOrxuSE43eomaCeSRQ6',
 }
 
 module.exports = twitterKeys;
